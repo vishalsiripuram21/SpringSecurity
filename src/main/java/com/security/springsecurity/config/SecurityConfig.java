1629a6121401dfb4f85bf332eefad1ba2a71cca8
Your Name <you@example.com>
2023-08-02 16:53:36 +0530

    Add Cors Policy

--- src/main/java/com/security/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/security/springsecurity/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.security.springsecurity.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -13,18 +17,38 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-      http.csrf().disable()
-              .authorizeHttpRequests((requests) ->
+      http.
+      cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+
+          @Override
+          public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+              CorsConfiguration corsObj = new CorsConfiguration();
+              corsObj.setAllowCredentials(true);
+              corsObj.setAllowedHeaders(Arrays.asList("*"));
+              corsObj.setAllowedMethods(Arrays.asList("*"));
+              corsObj.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+              corsObj.setMaxAge(3600L);
+              return corsObj;
+          }
+
+      }));
+
+      http.csrf(csrf -> csrf.ignoringRequestMatchers("*"))
+      .authorizeHttpRequests((requests) ->
                       requests.requestMatchers("/phello", "/name").authenticated()
                               .requestMatchers("/welcome", "/hello").permitAll()
-                              .requestMatchers(HttpMethod.POST,"/create").permitAll()
-                              )
+                              .requestMatchers(HttpMethod.POST, "/create").permitAll()
+      )
               .formLogin(Customizer.withDefaults())
               .httpBasic(Customizer.withDefaults());
     return http.build();
--- src/main/java/com/security/springsecurity/controller/Hello.java
+++ src/main/java/com/security/springsecurity/controller/Hello.java
@@ -1,16 +1,31 @@
 package com.security.springsecurity.controller;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.security.springsecurity.Entity.Customer;
+import com.security.springsecurity.Repository.CustomerRepo;
 
 @RestController
 public class Hello {
 
+  @Autowired
+  private CustomerRepo repo;
 
   @GetMapping("/hello")
-  public String hello(){
-    return "public hello controller";
+  public ResponseEntity<Customer> hello(){
+    Customer cust = repo.findByEmail("vishalsiripuram@gmail.com").orElseThrow();
+    return ResponseEntity.ok().cacheControl(CacheControl.maxAge(5,TimeUnit.SECONDS)).body(cust);
   }
   
 }
